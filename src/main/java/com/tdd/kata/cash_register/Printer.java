31d36c56841a5a07e4fbf2d994a5deaef5e46e97
Zhen She <neal.seah@gmail.com>
2020-08-09 19:16:08 +0800

    test: verify print by modify printer

--- src/main/java/com/tdd/kata/cash_register/Printer.java
+++ src/main/java/com/tdd/kata/cash_register/Printer.java
@@ -1,7 +1,10 @@
 package com.tdd.kata.cash_register;
 
 public class Printer {
+	public boolean hasBeenCalledPrint;
+
 	public void print(String content) throws PrinterOutOfPaperException{
+		hasBeenCalledPrint = true;
 		// send message to a real printer
 	}
 }

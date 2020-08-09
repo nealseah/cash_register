61660c9fd883f2058b68a3d23d0b19d2bc75e2bc
Zhen She <neal.seah@gmail.com>
2020-08-09 19:29:58 +0800

    test: process should throw HardwareException while printer is out of paper

--- src/test/java/com/tdd/kata/cash_register/CashRegisterTest.java
+++ src/test/java/com/tdd/kata/cash_register/CashRegisterTest.java
@@ -19,6 +19,21 @@ class CashRegisterTest {
 		assertEquals(stubContent, printer.hasBeenPrintWith);
 	}
 
+	@Test
+	void should_throws_hardware_exception_while_printer_is_out_of_paper() {
+		//given
+		StubPrinter printer = new StubPrinter();
+		CashRegister cashRegister = new CashRegister(printer);
+		final String stubContent = "content";
+		StubPurchase purchase = new StubPurchase(stubContent);
+		//when
+		//then
+		assertThrows(HardwareException.class, () -> {
+			cashRegister.process(purchase);
+		});
+	}
+
+
 	private class SpyPrinter extends Printer {
 		public boolean hasBeenCalledPrint;
 		public String hasBeenPrintWith;
@@ -41,6 +56,13 @@ class CashRegisterTest {
 		@Override
 		public String asString() {
 			return this.content;
+		}
+	}
+
+	private class StubPrinter extends Printer {
+		@Override
+		public void print(String content) throws PrinterOutOfPaperException {
+			throw new PrinterOutOfPaperException();
 		}
 	}
 }
